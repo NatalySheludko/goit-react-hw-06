edc144520404b08b72b4ac49ae7843a6127fc7ce
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-01 00:46:02 +0300

    add persist-redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
 import "modern-normalize";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
